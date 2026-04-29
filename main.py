18949ac18026325fa9c84dce4c75c6fe89af2829
MichauJ <janiszewski.mth@gmail.com>
2026-04-29 23:05:18 +0200

    data transformation completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.ds_e2e_project import logger
 from src.ds_e2e_project.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.ds_e2e_project.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
-
+from src.ds_e2e_project.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
@@ -22,6 +22,15 @@ except Exception as e:
          
          logger.exception(e)
          raise e
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.initiate_data_transformation()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<<<< \n\nx================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 def main():
     print("Hello from ds-e2e-project!")
